174b4e6d64c84aa6c5c894b55f4512425eee5ea6
SchmidtyNick <nick_schmidty@hotmail.com>
2017-10-20 15:51:20 -0600

    Customer class implementation

---
+++ src/Customer.java
@@ -0,0 +1,28 @@
+
+public class Customer {
+private String name;
+private String address;
+private float creditRating;
+private float discountRating;
+
+//CreditRating getter
+public float getCreditRating()
+{
+	return this.creditRating;
+}
+//Name Getter
+public String getName()
+{
+	return this.name;
+}
+//Address Getter
+public String getAddress()
+{
+	return this.address;
+}
+//DiscountRating getter
+public float getDiscountRating()
+{
+	return this.discountRating;
+}
+}
