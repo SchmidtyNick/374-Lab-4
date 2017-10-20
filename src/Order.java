fbd239fc12b219df9423fba1b1e571cf94c04c81
SchmidtyNick <nick_schmidty@hotmail.com>
2017-10-20 16:01:25 -0600

    Order and Product imnplementation

---
+++ src/Order.java
@@ -0,0 +1,19 @@
+import java.util.Date;
+public class Order {
+private Date dateReceieved;
+private double price;
+private int orderID;
+
+public Date getDateReceieved()
+{
+	return this.dateReceieved;
+}
+public double getPrice()
+{
+	return this.price;
+}
+public int getOrderID()
+{
+	return this.orderID;
+}
+}
---
+++ src/Product.java
@@ -0,0 +1,19 @@
+
+public class Product {
+private String name;
+private double price;
+private int productID;
+
+public String getName()
+{
+	return this.name;
+}
+public double getPrice()
+{
+	return this.price;
+}
+public int getProductID()
+{
+	return this.productID;
+}
+}
